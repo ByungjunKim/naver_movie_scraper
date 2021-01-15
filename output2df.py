15ce369d344de3096beef7cde864165047c632a0
ByungjunKim <kuntakim88@gmail.com>
2021-01-15 20:46:43 +0900

    기존 버전으로  merge

---
+++ output2df.py
@@ -0,0 +1,27 @@
+import ast
+import pandas as pd
+import glob
+import os
+from tqdm import tqdm
+
+file_name = input()
+
+def comments2df():
+    if file_name:
+      file_list = glob.glob(f'./output/comments/{file_name}')
+    else:
+      file_list = glob.glob(f'./output/comments/*')
+    df = pd.DataFrame()
+    for file in file_list:
+        print(file)
+        res_df = pd.DataFrame()
+        with open(file,'r') as f:
+            for line in tqdm(f):
+                res =  pd.DataFrame.from_dict([ast.literal_eval(line)])
+                res_df = res_df.append(res,ignore_index=True)
+            res_df['movie_idx']=os.path.basename(file)
+        df = df.append(res_df,ignore_index=True)
+    return df
+
+comments_df = comments2df()
+comments_df.to_csv(f'./output/comments_df/{file_list}_comments.csv',index=None)
